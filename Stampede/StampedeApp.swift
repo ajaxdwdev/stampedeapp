56f146bd5cb1135c1d6e9e5cdef3ff8028e84f3c
Ajaxdw <100229709+ajaxdwdev@users.noreply.github.com>
2024-01-25 10:40:30 -0500

    Initial Commit

---
+++ Stampede/StampedeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StampedeApp.swift
+//  Stampede
+//
+//  Created by Alexander Davila-Wollheim on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct StampedeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
